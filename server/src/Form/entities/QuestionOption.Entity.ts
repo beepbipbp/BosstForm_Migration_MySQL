a042ec82e4497ea3fee256ec2e358d0d86951761
beepbipbp <riskyjunk@gmail.com>
2023-02-10 22:32:06 +0900

    QuestionOption entity 작성

---
+++ server/src/Form/entities/QuestionOption.Entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+Entity();
+export default class QuestionOption {
+  @PrimaryGeneratedColumn()
+  question_option_id: number;
+
+  @Column({
+    type: "int",
+  })
+  fk_question_id: number;
+
+  @Column({
+    type: "varchar",
+    length: 50,
+  })
+  question_option_nmae: string;
+}
