b544f489f75d9820eeee992fef41dfc50e5dc445
beepbipbp <riskyjunk@gmail.com>
2023-02-10 16:37:09 +0900

    Form Entity에서 primary column 관련 데코레이터 변경

--- server/src/Form/Form.Model.ts
+++ server/src/Form/Form.Model.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export default class Form {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   form_id: number;
 
   @Column()
