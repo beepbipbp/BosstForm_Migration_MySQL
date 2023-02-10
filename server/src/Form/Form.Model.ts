d5da625396b7624c729e039a8d1a29466ee6cac5
beepbipbp <riskyjunk@gmail.com>
2023-02-10 17:06:05 +0900

    form entity에서 컬럼 세부설정 작성

--- server/src/Form/Form.Model.ts
+++ server/src/Form/Form.Model.ts
@@ -1,37 +1,65 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import CATEGORY_LIST from "./Form.enum";
 
 @Entity()
 export default class Form {
   @PrimaryGeneratedColumn()
   form_id: number;
 
-  @Column()
+  @Column({
+    type: "int",
+  })
   fk_user_id: number;
 
-  @Column()
+  @Column({
+    type: "varchar",
+    length: "50",
+    default: "제목 없음",
+  })
+  title: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: true,
+  })
   description: string;
 
-  @Column()
+  @Column({
+    type: "varchar",
+    length: 15,
+    enum: CATEGORY_LIST,
+    nullable: true,
+  })
   category: string;
 
-  @Column()
+  @Column({
+    type: "boolean",
+    default: false,
+  })
   accept_response: boolean;
 
-  @Column()
+  @Column({
+    type: "boolean",
+    default: false,
+  })
   on_board: boolean;
 
-  @Column()
+  @Column({
+    type: "boolean",
+    default: false,
+  })
   login_required: boolean;
 
-  @Column()
+  @Column({
+    type: "boolean",
+    default: false,
+  })
   response_modifiable: boolean;
 
-  @Column()
+  @Column({
+    type: "int",
+    default: 0,
+  })
   response_count: number;
-
-  @Column()
-  created_at: Date;
-
-  @Column()
-  updated_at: Date;
 }
