cb08273964c557b9646b63fa3ba691c72b02cfab
beepbipbp <riskyjunk@gmail.com>
2023-02-10 16:25:52 +0900

    사용하지 않는 mongoDB 관련 파일 삭제

--- server/src/Common/Utils/GetDateString.ts
+++
@@ -1,8 +0,0 @@
-function getDateString(date: Date): string {
-  const yearString = `${date.getFullYear()}`;
-  const monthString = date.getMonth() + 1 >= 10 ? `${date.getMonth() + 1}` : `0${date.getMonth() + 1}`;
-  const dateString = date.getDate() >= 10 ? `${date.getDate()}` : `0${date.getDate()}`;
-  return `${yearString}-${monthString}-${dateString}`;
-}
-
-export default getDateString;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,5 @@
 import app from "./app";
 import myDataSource from "./Loader/MySQL.Loader";
-import connectMongoDB from "./Loader/Mongo.Loader";
 
 myDataSource.initialize().then(() => {
   console.log("Data Source has been initialized!");
